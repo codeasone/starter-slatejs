526eea891ea5e9de435881a49f4b2d89ea7f88e0
Mark Stuart <codeasone@gmail.com>
2022-10-24 11:57:37 +0100

    Baseline minimal app with history (nice)

--- src/cljs/starter_slatejs/core.cljs
+++ src/cljs/starter_slatejs/core.cljs
@@ -1,52 +1,18 @@
 (ns starter-slatejs.core
-  (:require [medley.core :as m]
-            [slate :as slate :refer [Value]]
-            [slate-react :as slate-react]
-            [uix.compiler.alpha :as uixc]
-            [uix.core.alpha :as uix]
+  (:require [cljs-bean.core :refer [->js]]
+            [slate :as slate :refer [createEditor]]
+            [slate-history :as slate-history :refer [withHistory]]
+            [slate-react :as slate-react :refer [Slate Editable withReact]]
             [uix.dom.alpha :as uix.dom]))
 
-(def minimal-document
-  {:data {}
-   :nodes [{:data {},
-            :nodes [{:marks [], :object "text", :text "This is a test"}]
-            :object "block"
-            :type "p"}]
-   :object "document"})
-
-(defn document->value
-  [json]
-  (.fromJSON Value (clj->js {:document json})))
-
-(defn on-change
-  [editor _next]
-  (let [new-value (.-value editor)]
-    (prn "Change:" new-value)))
-
-(defn decorated-block-node
-  [tag {:keys [attributes comments _node children _isSelected _editor] :as _props}]
-  (prn "Rendering decorated block!")
-  [tag (js->clj attributes)
-   [:span
-    (when (#{:p} tag)
-      (str (js->clj comments)))]
-   children])
-
-(def paragraph
-  (fn [props]
-    (decorated-block-node :p props)))
-
-(def uix-component-lookup
-  {"p" paragraph})
-
-(def reactified-component-lookup (m/map-vals uixc/as-react uix-component-lookup))
+(def initial-value (->js [{:type "paragraph"
+                           :children [{:text "This is some text..."}]}]))
 
 (defn app
   []
-  (let [document-value (document->value minimal-document)
-        value* (uix/state document-value)]
-    [:> slate-react/Editor {:value @value*
-                            :onChange on-change}]))
+  [:> Slate {:editor (withHistory (withReact (createEditor)))
+             :value initial-value}
+   [:> Editable {:placeholder "Enter some plain text..."}]])
 
 (defn mount
   []
